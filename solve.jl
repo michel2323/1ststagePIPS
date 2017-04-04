7551d772930ce5273611f9e46d44ec534da9c6b8
Michel Schanen <mschanen@anl.gov>
2017-04-04 14:09:52 -0500

    Solving in Julia

---
+++ solve.jl
@@ -0,0 +1,31 @@
+
+println("Reading matrix M")
+fp=open("1ststageM.dmp")
+n=read(fp, Int32)
+println(n)
+A=read(fp, Float64, n,n)
+#println(A)
+close(fp)
+
+println("Reading RHS")
+fp=open("1ststageRHS.dmp")
+n=read(fp, Int32)
+println(n)
+rhs=read(fp, Float64, n)
+#println(rhs)
+close(fp)
+
+println("Reading solution")
+fp=open("1ststageSol.dmp")
+n=read(fp, Int32)
+println(n)
+sol=read(fp, Float64, n)
+close(fp)
+
+println("Solving system")
+x=\(A,rhs)
+
+println("Solution from file:")
+println(sol)
+println("Computed solution:")
+println(x)
