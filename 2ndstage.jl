3a617efd5d99e15ab35a5b16049756c13c9c3637
Michel Schanen <mschanen@anl.gov>
2017-08-02 14:06:32 -0500

    separate global and 1st stage matrix test

--- 2ndstage.jl
+++ 2ndstage.jl
@@ -5,7 +5,7 @@ using PyPlot
 # Format is m, n integers setting the sizeof
 # and m*n entries
 function openMat(filename)
-  if (isfile("filename"))
+  if (isfile(filename))
     fp=open(filename)
     m=read(fp, Int32)
     n=read(fp, Int32)
@@ -17,32 +17,6 @@ function openMat(filename)
     A=Array{Float64}(0,2)
   end
   return A
-end
-
-# Read first stage matrix and compute solution
-function firststage()
-  println("Reading matrix M")
-  A=openMat("1ststageM.dmp")
-  # show structure
-  #spy(A)
-  #show()
-  # Reading RHS  
-  println("Reading RHS")
-  rhs=openMat("1ststageRHS.dmp")
-  # Reading solution computed in PIPS  
-  println("Reading solution")
-  sol=openMat("1ststageSol.dmp")
-  # Solve system in Julia  
-  println("Solving system")
-  x=\(A,rhs)
-  
-  # Compare both solutions  
-  println("Solution from file:")
-  println(norm(sol))
-  println("Computed solution:")
-  println(norm(x))
-  
-  println("Condition number: ", cond(A,Inf))
 end
 
 # Read 1st stage block of the global matrix
